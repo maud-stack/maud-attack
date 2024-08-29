06e2f1f3c8130385766fce6e9fd373469e90da1e
maud-stack <bizahmadhaji@gmail.com>
2024-08-29 16:51:15 -0400

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,13 +1,1 @@
-Function Open-Website {
-    param (
-        [parameter(Mandatory=$True)]
-        [string]$Url
-    )
-    
-    # Open the URL in the default web browser
-    Start-Process $Url
-}
-
-# Open fvtal.com
-$website = "https://www.fvtal.com"
-Open-Website -Url $website
+Start-Process "https://www.fvtal.com"
